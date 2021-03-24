cc0a1d2ebbfc66d88150b17888c08ccda58ece6c
Michał Szymański <michal.szymanski@softable.pl>
2021-03-24 12:43:21 +0100

    # configuration - few changes in next.config and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,55 @@ module.exports = {
   plugins: [],
   darkMode: false,
   theme: {
-    colors: {},
-    extend: {},
+    colors: {
+      primary: '#060F12',
+      secondary: '#122025',
+      tertiary: '#59F5BC',
+      quaternary: '#EFF1F2',
+      white: '#FFFFFF',
+      black: '#000000',
+      red: {
+        100: '#FC7F88',
+        200: '#EB555F',
+        300: '#D53E48',
+        400: '#E63946',
+      },
+      yellow: {
+        100: '#FFECBE',
+        200: '#FDE19B',
+        300: '#F1CF7A',
+        400: '#FCAF58',
+      },
+      green: {
+        100: '#B0E9D7',
+        200: '#7FE3C3',
+        300: '#66D4B1',
+        400: '#2BA84A',
+      },
+      pink: {
+        100: '#E5B2C7',
+        200: '#E994B7',
+        300: '#DB759F',
+      },
+      blue: {
+        100: '#4D869C',
+        200: '#004C6A',
+        300: '#033b51',
+        400: '#033042',
+        500: '#022533',
+      },
+      gray: {
+        100: '#9296AD',
+        200: '#E8E8E8',
+        300: '#EFF1F2',
+        400: '#353535',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Source Sans Pro'],
+      },
+    },
   },
   variants: {
     extend: {},
